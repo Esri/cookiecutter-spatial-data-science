b4ee83bb82775bb86fe089d15a3d0982663a5bb0
Daniel Wilson <harenil@gmail.com>
2019-01-25 13:24:34 -0700

    fixed setup.py

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
-    author='{{ cookiecutter.author_names }}',
+    author='{{ cookiecutter.author_name }}',
     license='{{ cookiecutter.license }}',
 )
