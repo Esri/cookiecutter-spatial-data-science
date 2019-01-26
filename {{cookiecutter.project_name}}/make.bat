902318cb84ae219e3013c22933813304bff8e427
Daniel Wilson <harenil@gmail.com>
2019-01-25 17:24:12 -0700

    changed make files to add a make_kernel command and changed some variables

--- {{cookiecutter.project_name}}/make.bat
+++ {{cookiecutter.project_name}}/make.bat
@@ -4,8 +4,9 @@
 
 SETLOCAL
 SET PROJECT_DIR=%cd%
-SET PROJECT_NAME=geoai_project
-SET CONDA_PARENT=arcgispro-py3
+SET PROJECT_NAME="{{ cookiecutter.project_name }}"
+SET ENV_NAME={{ cookiecutter.conda_environment_name }}
+SET CONDA_PARENT={{ cookiecutter.conda_parent_environment }}
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: COMMANDS                                                                     :
@@ -15,24 +16,24 @@ SET CONDA_PARENT=arcgispro-py3
 GOTO %1
 
 :data
-	activate %PROJECT_NAME%
+	activate %ENV_NAME%
 	python src/data/make_dataset.py
 	ECHO ">>> Data processed."
 	EXIT
 	
 :: Create the local environment by cloning the parent environment
 :env_create
-	conda create --yes --name %PROJECT_NAME% --clone %CONDA_PARENT%
-	ECHO ">>> New conda environment, %PROJECT_NAME%, created. Activate with:"
+	conda create --yes --name %ENV_NAME% --clone %CONDA_PARENT%
+	ECHO ">>> New conda environment, %ENV_NAME%, created. Activate with:"
 	ECHO.
-	ECHO "- activate %PROJECT_NAME%"
+	ECHO "- activate %ENV_NAME%"
 	ECHO.
 	ECHO "- make env_activate"
 	EXIT
 	
 :: Export the current environment
 :env_export
-	conda env export --name %PROJECT_NAME% > environment.yml
+	conda env export --name %ENV_NAME% > environment.yml
 	ECHO ">>> %PROJECT_NAME% conda environment exported to ./environment.yml"
 	EXIT 
 	
@@ -40,6 +41,9 @@ GOTO %1
 :env_build
 	conda env create --yes -f environment.yml
 	EXIT
+
+:create_kernel 
+	python -m ipykernel install --user --name %ENV_NAME% --display-name "%PROJECT_NAME%"
 
 :: Run all tests in module
 :test
--- {{cookiecutter.project_name}}/scripts/make_kernel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m ipykernel install --user --name {{ cookiecutter.conda_environment_name }} --display-name "{{ cookiecutter.project_name }}"
