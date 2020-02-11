bccfd273ee09365af3368e8fa0c4bd01489fad51
Joel McCune <knu2xs@gmail.com>
2020-02-10 20:29:11 -0800

    Add cleanup to aprx xml to remove cookiecutter references

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,10 @@
 import os
+from pathlib import Path
+import re
 import shutil
+import tempfile
 import importlib
+from zipfile import ZipFile
 
 # see if arcpy available to accommodate non-windows environments
 try:
@@ -16,6 +20,59 @@ except AttributeError:
     else:
         has_arcpy = False
 
+
+def _cleanup_cookiecutter_references_in_aprx(aprx_path: Path):
+    """Clean up lingering references to cookiecutter toolbox and file geodatabase in the aprx."""
+
+    # ensure we're working with a pathlib.Path object
+    aprx_path = str(aprx_path) if isinstance(aprx_path, Path) else aprx_path
+
+    # temporary storage location for extracting contents of aprx to work with
+    tmp_dir = tempfile.mkdtemp()
+
+    # extract everything from the aprx to the temp directory
+    with ZipFile(aprx_path, 'r') as aprx:
+        aprx.extractall(tmp_dir)
+
+    xml_pro_name = 'GISProject.xml'
+
+    # string path to where file resides we are going to be modifying along with the output modified file
+    xml_pro_pth = os.path.join(os.path.abspath(tmp_dir), xml_pro_name)
+    tmp_pth = xml_pro_pth.replace('.xml', '_modified.xml')
+
+    # regular expression used to rip out the unneeded references to the original resources
+    regex = re.compile(
+        r"<CIMProjectItem xsi:type=\"typens:CIMProjectItem\"><CatalogPath>[\.\\/a-zA-Z{}\-_]*?cookiecutter\.[a-zA-Z]{3}<\/CatalogPath>.*?<\/CIMProjectItem>")
+
+    # open and read the original xml file
+    with open(tmp_pth, 'w') as out_file:
+        # open the temp file where the modifications will be saved
+        for line in open(xml_pro_pth, 'r'):
+            # drop any matching strings in the line
+            line = regex.sub(line, '') if regex.match(line) else line
+
+            # write the line to the output file
+            out_file.write(line)
+
+    # remove the original file and save the updated one in it's place
+    os.remove(xml_pro_pth)
+    os.rename(tmp_pth, xml_pro_pth)
+
+    # remove the original aprx file
+    os.remove(aprx_path)
+
+    # zip up the resources in the temp directory to create the new aprx archive
+    aprx_zip = shutil.make_archive(aprx_path, 'zip', tmp_dir)
+
+    # rename the file from a zip file to the orignial aprx name
+    os.rename(aprx_zip, aprx_path)
+
+    # remove the temp directory
+    shutil.rmtree(tmp_dir)
+
+    return aprx_path
+
+
 # if the cookiecutter.gdb exists, get rid of it
 gdb_ck = os.path.join(os.getcwd(), 'arcgis', 'cookiecutter.gdb')
 if os.path.exists(gdb_ck):
@@ -28,7 +85,7 @@ for drctry in dir_lst:
     if not os.path.exists(drctry):
         os.makedirs(drctry)
 
-# if arcpy available
+# if arcpy available, set up arcgis pro resources for the project
 if has_arcpy:
 
     # create project location path strings
@@ -72,6 +129,9 @@ if has_arcpy:
     # now delete the old toolbox and project
     os.remove(old_aprx.defaultToolbox)
     os.remove(existing_project_path)
+
+    # clean out any lingering Catalog references to cookiecutter resources
+    _cleanup_cookiecutter_references_in_aprx(new_project_path)
 
 # if arcpy is not available get rid of the arcgis directory, but leave everything else
 else:
