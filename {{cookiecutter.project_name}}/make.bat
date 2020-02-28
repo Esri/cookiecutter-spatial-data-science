72e807e81968e8aa943a60a834959eb8ea5f6970
Joel McCune <knu2xs@gmail.com>
2020-02-28 15:51:35 -0800

    Update version in make.bat

--- {{cookiecutter.project_name}}/make.bat
+++ {{cookiecutter.project_name}}/make.bat
@@ -83,7 +83,7 @@ GOTO %1
 
         :: Additional steps for the map widget to work in Jupyter Lab
         CALL jupyter labextension install @jupyter-widgets/jupyterlab-manager -y
-        CALL jupyter labextension install arcgis-map-ipywidget@1.7.0 -y
+        CALL jupyter labextension install arcgis-map-ipywidget@1.7.1 -y
     )
     EXIT /B
 
