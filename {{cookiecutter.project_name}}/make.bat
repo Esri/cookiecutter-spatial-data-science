f2f2fdfbc33a74cd2384e5f6f3dd2fc69d776ab7
Joel McCune <knu2xs@gmail.com>
2020-07-31 14:34:22 -0700

    update to python api 1.8.2

--- {{cookiecutter.project_name}}/make.bat
+++ {{cookiecutter.project_name}}/make.bat
@@ -73,7 +73,7 @@ GOTO %1
 
         :: Additional steps for the map widget to work in Jupyter Lab
         CALL jupyter labextension install @jupyter-widgets/jupyterlab-manager -y
-        CALL jupyter labextension install arcgis-map-ipywidget@1.8.1 -y
+        CALL jupyter labextension install arcgis-map-ipywidget@1.8.2 -y
 
         :: Set the ArcGIS Pro Python environment
         proenv "%ENV_NAME%"
