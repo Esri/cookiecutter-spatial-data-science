921cde85f382a0d0f1df461f6e599f261985181b
Joel McCune <knu2xs@gmail.com>
2020-08-04 21:56:18 -0700

    Get setup working

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
 setup(
     name='{{ cookiecutter.support_library }}',
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages('src'),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
+    long_description=long_description,
     author='{{ cookiecutter.author_name }}',
     license='{{ cookiecutter.open_source_license }}',
 )
