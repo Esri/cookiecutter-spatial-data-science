229cdac86f15e200eb611c2d914109f1936c5198
Joel McCune <knu2xs@hotmail.com>
2022-03-09 11:28:16 -0800

    Bump version: 0.0.0 → 0.1.0-dev0

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -31,7 +31,7 @@ copyright = '2021, Esri'
 author = 'Joel McCune'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.1.0-dev0'
 
 # -- General configuration -----------------------------------------------------
 
