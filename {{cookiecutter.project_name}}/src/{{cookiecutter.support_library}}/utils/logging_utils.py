1f89f33e6927a456c37c18012844f4da26b89747
Joel McCune <knu2xs@gmail.com>
2025-08-01 10:04:39 -0700

    Remove notebook copy and updates to template Python

--- {{cookiecutter.project_name}}/src/{{cookiecutter.support_library}}/utils/logging_utils.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.support_library}}/utils/logging_utils.py
@@ -2,8 +2,6 @@ import logging
 from pathlib import Path
 from typing import Union, Optional
 
-import pandas as pd
-
 from .main import has_arcpy
 
 __all__ = ["configure_logging", "format_pandas_for_logging"]
@@ -15,16 +13,18 @@ class ArcpyHandler(logging.Handler):
     DEBUG and INFO logging messages are be handled by the AddMessage method. WARNING logging messages are handled
     by the AddWarning method. ERROR and CRITICAL logging messages are handled by the AddError method.
     Basic use consists of the following.
-    .. code-block:: python
-        logger = logging.getLogger('arcpy-logger')
-        logger.setLevel('INFO')
-        ah = ArcpyHandler()
-        logger.addHandler(ah)
-        logger.debug('nauseatingly detailed debugging message')
-        logger.info('something actually useful to know')
-        logger.warning('The sky may be falling')
-        logger.error('The sky is falling.)
-        logger.critical('The sky appears to be falling because a giant meteor is colliding with the earth.')
+    
+    ``` python
+    logger = logging.getLogger('arcpy-logger')
+    logger.setLevel('INFO')
+    ah = ArcpyHandler()
+    logger.addHandler(ah)
+    logger.debug('nauseatingly detailed debugging message')
+    logger.info('something actually useful to know')
+    logger.warning('The sky may be falling')
+    logger.error('The sky is falling.)
+    logger.critical('The sky appears to be falling because a giant meteor is colliding with the earth.')
+    ```
     """
 
     # since everything goes through ArcPy methods, we do not need a message line terminator
@@ -45,7 +45,8 @@ class ArcpyHandler(logging.Handler):
         """
         Args:
             record: Record containing all information needed to emit a new logging event.
-        .. note::
+        
+        Note:
             This method should not be called directly, but rather enables the ``Logger`` methods to
             be able to use this handler correctly.
         """
@@ -78,30 +79,30 @@ def configure_logging(
     Get Python :class:`Logger<logging.Logger>` configured to provide stream, file or, if available, ArcPy output.
     The way the method is set up, logging will be routed through ArcPy messaging using :class:`ArcpyHandler` if
     ArcPy is available. If ArcPy is *not* available, messages will be sent to the console using a
-    :class:`StreamHandler<logging.StreamHandler>`. Next, if the ``logfile_path`` is provided, log messages will also
+    :class:`StreamHandler<logging.StreamHandler>`. Next, if the `logfile_path` is provided, log messages will also
     be written to the provided path to a logfile using a :class:`FileHandler<logging.FileHandler>`.
 
-    Valid ``log_level`` inputs include:
-    * ``DEBUG`` - Detailed information, typically of interest only when diagnosing problems.
-    * ``INFO`` - Confirmation that things are working as expected.
-    * ``WARNING`` or ``WARN`` -  An indication that something unexpected happened, or indicative of some problem in the
-            near future (e.g. ‘disk space low’). The software is still working as expected.
-    * ``ERROR`` - Due to a more serious problem, the software has not been able to perform some function.
-    * ``CRITICAL`` - A serious error, indicating that the program itself may be unable to continue running.
+    Valid `log_level` inputs include:
+    * `DEBUG` - Detailed information, typically of interest only when diagnosing problems.
+    * `INFO` - Confirmation that things are working as expected.
+    * `WARNING` or ``WARN`` -  An indication that something unexpected happened, or indicative of some problem in the
+        near future (e.g. "disk space low"). The software is still working as expected.
+    * `ERROR` - Due to a more serious problem, the software has not been able to perform some function.
+    * `CRITICAL` - A serious error, indicating that the program itself may be unable to continue running.
 
     Args:
         level: Logging level to use. Default is `'INFO'`.
         logfile_path: Where to save the logfile if file output is desired.
 
-    .. code-block:: python
-
-        # only output to console and potentially Pro if ArcPy is available
-        configure_logging('DEBUG')
-        logging.debug('nauseatingly detailed debugging message')
-        logging.info('something actually useful to know')
-        logging.warning('The sky may be falling')
-        logging.error('The sky is falling.)
-        logging.critical('The sky appears to be falling because a giant meteor is colliding with the earth.')
+    ``` python
+    # only output to console and potentially Pro if ArcPy is available
+    configure_logging('DEBUG')
+    logging.debug('nauseatingly detailed debugging message')
+    logging.info('something actually useful to know')
+    logging.warning('The sky may be falling')
+    logging.error('The sky is falling.)
+    logging.critical('The sky appears to be falling because a giant meteor is colliding with the earth.')
+    ```
 
     """
     # ensure valid logging level
@@ -157,21 +158,22 @@ def configure_logging(
 
 
 def format_pandas_for_logging(
-    pandas_df: pd.DataFrame, title: str, line_tab_prefix="\t\t"
+    pandas_df: "pd.DataFrame", title: str, line_tab_prefix="\t\t"
 ) -> str:
     """
     Helper function facilitating outputting a :class:`Pandas DataFrame<pandas.DataFrame>` into a logfile. This function only
         formats the data frame into text for output. It should be used in conjunction with a logging method.
 
-    .. code-block:: python
-
-        logging.info(format_pandas_for_logging(df, title='Summary Statistics'))
+    ``` python
+    logging.info(format_pandas_for_logging(df, title='Summary Statistics'))
+    ```
 
     Args:
         pandas_df: Pandas ``DataFrame`` to be converted to a string and included in the logfile.
         title: String title describing the data frame.
         line_tab_prefix: Optional string comprised of tabs (``\\t\\t``) to prefix each line with providing indentation.
     """
+    import pandas as pd
     log_str = line_tab_prefix.join(pandas_df.to_string(index=False).splitlines(True))
     log_str = f"{title}:\n{line_tab_prefix}{log_str}"
     return log_str
